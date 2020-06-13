5abe320b535115dc045d996ae90de24c0f9cc7dc
kyungjong-903 <roroh0903@gmail.com>
2020-06-13 17:16:05 +0900

    토큰 생성중 오류수정 완료 및 권한 체크 로그아웃 기능 구현

--- home.js
+++ home.js
@@ -5,6 +5,7 @@ const port = 8080
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const { User } = require('./models/user');
+const { auth } = require('./middleware/auth');
 const config = require('./config/key');
 
 
@@ -21,7 +22,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!ㅎㅇㅎㅇㅎ'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     const user = new User(req.body);
 
@@ -33,7 +34,8 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
+
     User.findOne({ email: req.body.email }, (err, userInfo) => {
         if (!userInfo){
             return res.json({
@@ -41,24 +43,42 @@ app.post('/login', (req, res) => {
                 message: "제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
-        console.log("user : " + userInfo);
-        userInfo.comparePassword( req.body.password, (err, isMatch) => {
-            console.log("asd");
-            if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
-
+        userInfo.comparePassword( req.body.password, ( err, isMatch ) => {
+            if(!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
             userInfo.generateToken((err, user) => {
                 if(err) return res.status(400).send(err);
 
                 //토큰을 저장함 어디에? 나는 세션에 하고싶은데 쿠키에 한다고 한다
                 res.cookie("some_auth", user.token)
                 .status(200).json({loginSuccess: true, userId: user._id})
-
-
-
             })
-        })
+        }) 
     })
 })
+app.get('/api/users/auth', auth , (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true, // 관리자 상태 
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        image: req.user.image,
+        role: req.user.role
+    })
+})
+
+app.get('/api/users/logout', auth , (req, res) =>{
+    User.findOneAndUpdate({_id: req.user._id},
+        { token: "" },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        maxlength: 100
     },
     role: {
         type: Number,
@@ -49,10 +49,13 @@ userSchema.pre('save', function( next ){
     }
 })
 
-userSchema.methods.comparePassword = function(planePassword, cb){
-    bcrypt.compare(planePassword, this.password, function(err, isMatch){
-        if(err) return cb(err),
-        cb(null, isMatch)
+
+userSchema.methods.comparePassword = function( planePassword, cb ){
+    var user = this;
+    
+    bcrypt.compare( planePassword, user.password, function(err, isMatch ){
+        if( err ) return cb( err )
+        cb( null, isMatch )
     })
 }
 
@@ -65,11 +68,28 @@ userSchema.methods.generateToken = function(cb){
     // ->
     // 'secretToken' -> user._id
     user.token = token
-user.save(function(err, user){
-    if(err) return cb(err)
-    cb(null, user)
-})
 
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+
+}
+
+userSchema.statics.findByToken = function( token, cb) {
+    var user = this;
+
+    //토큰을 디코딩 하는중
+
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 뒤
+        // 클라이언트에서 가져온 토큰이 DB에 저장된 토큰과 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
 }
 
 const User = mongoose.model('User', userSchema)
